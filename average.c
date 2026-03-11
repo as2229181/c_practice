00b27ac8842065b89ab16df1e7a6aee532c15383
erickuo <as2229181@gmail.com>
2026-03-11 20:54:35 +0800

    practice while to calculate average

---
+++ average.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main() {
+    int nunber;
+    int sum = 0;
+    int count = 0;
+    printf("Please enter number to calculate average: (0 to quit)\n");
+    scanf("%d", &nunber);
+    while (nunber != 0) {
+        sum += nunber;
+        count ++;
+        scanf("%d", &nunber);
+    }
+   if (count != 0) {
+       printf("The average is %f\n", (float)sum / count);
+   }
+   else {
+       printf("The average is N/A.\n");
+   }
+   return 0;
+}
